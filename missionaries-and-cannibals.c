488cde6aeab63e1858107d2aab0a537336f56b3e
root <atrautrim7@gmail.com>
2019-05-29 16:51:38 -0400

    reformated problem representation

--- missionaries-and-cannibals.c
+++ missionaries-and-cannibals.c
@@ -9,33 +9,36 @@
  * missionaries will get eaten.
  */
 
-#define N 6
+#define N 2
 
 /* Solution functions */
-int breadth_first_search(int bank1[N], int bank2[N]);
-int depth_first_search(int start[N], int from[N], int to[N]);
-int iterative_deepening_search(int bank1[N], int bank2[N]);
+int depth_first_search(int start[], int goal[], int visited[32][4], int path[32][4], int depth);
 
 /* Utility functions */
-int cross(int from[N], int to[N], int i, int j);
-int is_empty(int bank[N]);
-int find_empty_position(int arr[N]);
-int get_next(int start, int bank[N]);
-int valid_state(int bank[N]);
+int cross(int from[], int to[], int i, int j);
+int is_empty(int arr[]);
+int is_new_state(int start[], int end[], int visited[32][4]);
+int valid_state(int bank[]);
 
-void print_banks(int bank1[N], int bank2[N]);
+void add_state(int start[], int end[], int visited[32][4]);
+void copy(int copy[], int org[]);
+void print_banks(int start[], int end[]);
 
 int main(void) {
 
 	/* 
 	 * bank1 is the initial starting state of everyone
 	 * bank2 is the end goal state of everyone
-	 * -1 represents a cannibal, 0 no one, and 1 a missionary
+	 * 3 missionaries and 3 cannibals
+	 * index 0 are missionaries
+	 * index 1 are cannibals
 	 */
-	int bank1[N] = {-1, -1, -1, 1, 1, 1};
-	int bank2[N] = {0, 0, 0, 0, 0, 0};
+	int start[] = {3, 3};
+	int end[] = {0, 0};
+	int visited[32][4];
+	int path[32][4];
 
-	print_banks(bank1, bank2);
+	depth_first_search(start, end, visited, path, 0);
 
 	return 1;
 }
@@ -43,20 +46,64 @@ int main(void) {
 /*
  * Recursive solution by depth-first search
  */
-int depth_first_search(int start[N], int from[N], int to[N]) {
+int depth_first_search(int start[], int end[], int visited[32][4], int path[32][4], int depth) {
 
-	// base case, solution found
-	if (is_empty(start)) {
+	print_banks(start, end);
+	add_state(start, end, visited);
+
+	// start position is empty and solution has been found
+	if (start[0] == 0 && start[1] == 0) {
 		return 1;
 	}
-	
-	int first;
-	int second;
-	for (int i = 0; i < N; ++i) {
-		first = get_next(i, from);
-		for (int j = first; j < N && (second = get_next(j, from)) != -1; ++j);
+
+	// copies values incase we need to reset
+	int temp1[N];
+	int temp2[N];
+	copy(temp1, start);
+	copy(temp2, end);
+
+	// determines which direction to go
+	int *from;
+	int *to;
+	if (depth % 2 == 0) {
+		from = start;
+		to = end;
+	} else {
+		from = end;
+		to = start;
 	}
 
+	depth++;
+	// attempts to find solution from current state
+	// returns 1 on success
+	// resets values otherwise
+	if (cross(from, to, 0, 0) && is_new_state(start, end, visited) && (depth_first_search(start, end, visited, path, depth))) { 
+		return 1;
+	}
+	copy(start, temp1);
+	copy(end, temp2);
+
+	if (cross(from, to, 1, 1) && is_new_state(start, end, visited) && (depth_first_search(start, end, visited, path, depth)))
+		return 1;
+	copy(start, temp1);
+	copy(end, temp2);
+
+	if (cross(from, to, 0, 1) && is_new_state(start, end, visited) && (depth_first_search(start, end, visited, path, depth)))
+		return 1;
+	copy(start, temp1);
+	copy(end, temp2);
+	
+	if (cross(from, to, 0, -1) && is_new_state(start, end, visited) && (depth_first_search(start, end, visited, path, depth)))
+		return 1;
+	copy(start, temp1);
+	copy(end, temp2);
+	
+	if (cross(from, to, 1, -1) && is_new_state(start, end, visited) && (depth_first_search(start, end, visited, path, depth)))
+		return 1;
+	copy(start, temp1);
+	copy(end, temp2);
+
+	// unsuccessful
 	return 0;
 }
 
@@ -64,28 +111,22 @@ int depth_first_search(int start[N], int from[N], int to[N]) {
  * person(s) cross the river
  * returns 1 on success, 0 otherwise
  */
-int cross(int from[N], int to[N], int i, int j) {
+int cross(int from[], int to[], int i, int j) {
 
-	int index1 = find_empty_position(to);
-	to[index1] = from[i];
-	from[i] = 0;
-
-	// second person is optional
-	int index2;
+	from[i] -= 1;
+	to[i] += 1;
 	if (j != -1) {
-		index2 = find_empty_position(to);
-		to[index2] = from[j];
-		from[j] = 0;
+		from[j] -= 1;
+		to[j] += 1;
 	}
 
-	// checks state for validity
-	if (valid_state(from) == 0 || valid_state(to) == 0) {
-		// resets position for invalid states
-		from[i] = to[index1];
-		to[index1] = 0;
+	// checks if the cross is valid
+	if (!valid_state(from) || !valid_state(to)) {
+		from[i] += 1;
+		to[i] -= 1;
 		if(j != -1) {
-			from[j] = to[index2];
-			to[index2] = 0;
+			from[j] += 1;
+			to[j] += 1;
 		}
 		return 0;
 	}
@@ -93,71 +134,64 @@ int cross(int from[N], int to[N], int i, int j) {
 	return 1;
 }
 
-/* 
- * used to check if solution is found 
- * returns 1 on success, 0 otherwise
- */
-int is_empty(int bank[N]) {
-
-	for (int i = 0; i < N; ++i)
-		if (bank[i] != 0)
+int is_empty(int arr[]) {
+	for (int i = 0, n = sizeof(arr)/sizeof(arr[0]); i < n; ++i)
+		if (arr[i] != 0)
 			return 0;
 	return 1;
 }
 
-/* returns the index of the next available position, -1 otherwise */
-int find_empty_position(int arr[N]) {
-	for (int i = 0; i < N; ++i)
-		if (arr[i] == 0)
-			return i;
-	return -1;
-}
-
-/*
- * return index of next person
- * -1 if no valid persons
- */
-int get_next(int start, int bank[N]) {
-
-	for (int i = start; i < N; ++i)
-		if (bank[i] != 0)
-			return i;
-	return -1;
+/* checks if current state has already been visited */
+int is_new_state(int start[], int end[], int visited[32][4]) {
+	for (int i = 0; i < 32; ++i) {
+		if (start[0] == visited[i][0] && start[1] == visited[i][1] && end[0] == visited[i][2] && end[1] == visited[i][3])
+			return 0;
+		else if (is_empty(visited[i]))
+			return 1;
+	}
+	return 1;
 }
 
 /* 
  * verifys if a state is valid
- * i.e. a missionary is not outnumbered by cannibals
+ * i.e. missionaries are not outnumbered by cannibals
  * 1 is valid, 0 is not
  */
-int valid_state(int bank[N]) {
-	int c_count = 0;
-	int m_count = 0;
-	for (int i = 0; i < N; ++i) {
-		if (bank[i] == -1)
-			c_count++;
-		else if(bank[i] == 1)
-			m_count++;
-	}
-	// if cannibals exceed missionaries
-	if (c_count > m_count)
+int valid_state(int bank[]) {
+	if (bank[0] < bank[1] && bank[0] != 0)
 		return 0;
 	return 1;
 }
 
 /*
+ * adds a state to the visited array
+ */
+void add_state(int start[], int end[], int visited[32][4]) {
+	for (int i = 0; i < 32; ++i) {
+		if (is_empty(visited[i])) {
+			visited[i][0] = start[0];
+			visited[i][1] = start[1];
+			visited[i][2] = end[0];
+			visited[i][3] = end[1];
+			return;
+		}
+	}
+}
+
+/*
+ * copies vale from one array into the other
+ */
+void copy(int copy[], int org[]) {
+	for (int i = 0; i < N; ++i)
+		copy[i] = org[i];
+	return;
+}
+
+/*
  * Testing function
  */
-void print_banks(int bank1[N], int bank2[N]) {
-	
-	printf("Starting bank:");
-	for(int i = 0; i < N; ++i) {
-		printf(" %d", bank1[i]);
-	}
-	printf("\nEnding bank:");
-	for(int i = 0; i < N; ++i) {
-		printf(" %d", bank2[i]);
-	}
-	printf("\n");
+void print_banks(int start[], int end[]) {
+ 	fprintf(stdout, "Start: %d %d\n", start[0], start[1]);
+	fprintf(stdout, "End:   %d %d\n\n", end[0], end[1]);
 	return;
 }
